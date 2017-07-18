820951e0a1a32562c31232b63112bef9ba49f6f1
Neil Locketz <neil@nlocketz.com>
2017-07-17 20:01:37 -0400

    Added source encoding, and also checked random results

--- src/it/abstract-class-it/src/test/java/com/example3/RandomIT.java
+++ src/it/abstract-class-it/src/test/java/com/example3/RandomIT.java
@@ -6,5 +6,7 @@ public class RandomIT {
     @Test
     public void testRandom() {
         RandomGenerator r = RandomGeneratorRegistry.getRandomGeneratorByName("JAVA");
+        // Seeded this happens to be the correct output.
+        Assert.assertEquals(-1157408321, r.generate(5));
     }
 }
